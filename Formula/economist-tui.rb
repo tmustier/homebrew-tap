2206afd9f4830afc293ef0dfb6b1106b7d8c670f
Thomas Mustier <mustierthomas@gmail.com>
2026-01-22 19:42:54 +0000

    chore(release): economist-tui 0.3.2

--- Formula/economist-tui.rb
+++ Formula/economist-tui.rb
@@ -1,8 +1,8 @@
 class EconomistTui < Formula
   desc "Terminal UI to browse and read The Economist"
   homepage "https://github.com/tmustier/economist-tui"
-  url "https://github.com/tmustier/economist-tui/archive/refs/tags/v0.3.1.tar.gz"
-  sha256 "bedfaecf9009d4c1a934a32b93d339cb58c59d55d6895a19017ca455c1ea9538"
+  url "https://github.com/tmustier/economist-tui/archive/refs/tags/v0.3.2.tar.gz"
+  sha256 "69c0b7a8ec68a7b89f450bbcd10fc267ee274398e743eae3467c45438d3bb93f"
   license "MIT"
 
   depends_on "go" => :build
